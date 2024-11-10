366598004fb5a0f7929ac2b6c4a49a80a2a2c718
Jack Case <jackacase@gmail.com>
2024-11-10 08:34:01 -0500

    Initialized Wagtail site with devcontainer

---
+++ ms_kiersten/ms_kiersten/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-+q#gib@fs8zp790g!-je8h#y$cbp)fxxiptq^6f=3+t-mure6n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
