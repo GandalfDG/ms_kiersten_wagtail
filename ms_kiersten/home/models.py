23e6d1a2193f38efff9ef74ba91a85ec213ea829
Jack Case <jackacase@gmail.com>
2024-11-10 14:11:06 +0000

    created basic homepage model

--- ms_kiersten/home/models.py
+++ ms_kiersten/home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    headline = RichTextField()
+    biography = RichTextField()
+    
+    updated = models.DateField(auto_now=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('headline'),
+        FieldPanel('biography')
+    ]
