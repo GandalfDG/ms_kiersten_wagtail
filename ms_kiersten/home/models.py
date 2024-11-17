018687d85f9c1f8a72981c8e53665677d1c5d398
Jack Case <jackacase@gmail.com>
2024-11-17 19:55:01 +0000

    allow the HomePageMenuLink model to hold a URL or a page reference

--- ms_kiersten/home/models.py
+++ ms_kiersten/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.admin.panels import FieldPanel, InlinePanel, FieldRowPanel
 
 from modelcluster.fields import ParentalKey
 from wagtail.models import Orderable
@@ -24,6 +24,19 @@ class HomePageMenuLink(Orderable):
     page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='home_menu_links')
     
     name = models.TextField()
-    url = models.URLField()
-
     boxicons_icon_name = models.TextField()
+    
+    # The link can either be to an external URL
+    external_url = models.TextField(null=True, blank=True)
+    # or to an internal page
+    page_link = models.ForeignKey("wagtailcore.Page", null=True, blank=True, on_delete=models.CASCADE)
+
+    external_url_panel = FieldPanel('external_url')
+    internal_page_panel = FieldPanel('page_link')
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('boxicons_icon_name'),
+        FieldRowPanel((external_url_panel, internal_page_panel))
+
+    ]
