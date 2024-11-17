e156783fa7058b670d4e2c9d2e37fd58081f067b
Jack Case <jackacase@gmail.com>
2024-11-17 07:37:56 -0500

    working on homepage model

--- ms_kiersten/home/models.py
+++ ms_kiersten/home/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
+
+from modelcluster.fields import ParentalKey
+from wagtail.models import Orderable
 
 
 class HomePage(Page):
@@ -13,5 +16,14 @@ class HomePage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('headline'),
-        FieldPanel('biography')
+        InlinePanel('home_menu_links', heading='Menu Links', label='link'),
+        FieldPanel('biography'),
     ]
+
+class HomePageMenuLink(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='home_menu_links')
+    
+    name = models.TextField()
+    url = models.URLField()
+
+    boxicons_icon_name = models.TextField()
