5c04c3569668e6bffa3f6b4899fff1407c658029
Jack Case <jackacase@gmail.com>
2024-12-30 20:07:56 +0000

    work on production settings file

--- ms_kiersten/ms_kiersten/settings/production.py
+++ ms_kiersten/ms_kiersten/settings/production.py
@@ -1,4 +1,6 @@
 from .base import *
+import dotenv
+from os import environ
 
 DEBUG = False
 
@@ -6,3 +8,10 @@ try:
     from .local import *
 except ImportError:
     pass
+
+STATIC_ROOT = "/var/www/msk_static"
+MEDIA_ROOT = "/var/www/msk_media"
+
+dotenv.load_dotenv("../msk.env")
+
+SECRET_KEY = environ["DJANGO_SECRET_KEY"]
